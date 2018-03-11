aa49510a706c65f1bb3c9da90ce1e164e8bc38c3
jmarz3 <jmarz3@ml1.net>
2018-03-11 13:43:21 -0400

    minor update to program. Comment added.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+    // This comment is a statement indicating a change to the original code.
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
